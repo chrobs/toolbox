58a8ba3fa6cfef77e9635aa652174a67d67c6aa7
tempo <github@tempo-tm.de>
2013-08-09 16:46:14 +0200

    first working inotify backup script

--- backup_copy_inotify.sh
+++ backup_copy_inotify.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-inotifywait -m -e delete -e close_write --exclude '\*.sw??$' . |
-while read dir ev file; do
-  if [ "$ev" = "DELETE" ]; then
-    echo "$file" >> /inotify.log
-  else
-    cp ./"$file" zinot/"$file"
-  fi
+# backup file if modified
+#
+# toolbox@tempo-tm.de
+# created: 2013-08-09
+# version: 0.1
+#
+# script uses inotify-tools and zenity file chooser
+
+
+fullpath=`zenity --file-selection --title="Zu überwachende Datei"`
+filename=$(basename "$fullpath")
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+dir=`zenity --file-selection --directory --title="Zielverzeichnis, in das die Kopien gespeichert werden"`
+
+while inotifywait -e modify "$fullpath"; do
+  datetime=`date +%Y-%m-%d_%H:%M:%S`
+  target="$filename-$datetime.$extension"
+  cp $fullpath $dir/$target
 done
